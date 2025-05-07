77db42b90e533d5405d73fe5defb595ecb06aeb3
anushaprao <122496338+anushaprao@users.noreply.github.com>
2025-05-06 21:01:48 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Anusha Rao
+
+Explore Answers:
+1. I would not use a unit test to test the message feature on a messaging application. This is because a con of unit tests is that they do not test how individual components interact on the application/feature level, and this is an application level requirement.
+2. Yes, I would use a unit test to test the "max message length" feature of a messaging application. This is because this would be a simple function testing an individual component rather than a feature level design, or an application level requirement.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const textInput = document.getElementById('text-to-speak');
+  const talkButton = document.querySelector('button');
+  const faceImg = document.querySelector('img');
+
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = '';
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  // Some browsers load voices asynchronously
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voices[voiceSelect.value];
+    utterance.voice = selectedVoice;
+
+    utterance.onstart = () => {
+      faceImg.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      faceImg.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterance);
+  });
 }
